0b33611376d8fe554b5656e01ee2032a6b4f882e
VishalRohilla07 <Rohillavishal12@gmail.com>
2021-09-13 11:35:16 +0530

    added the funtionality of unfavourite and chaning the tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,10 @@
 
 // action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_To_FAVOURITE = "ADD_TO_FAVOURITE";
+export const REMOVE_FROM_FAVOURITE = "REMOVE_FROM_FAVOURITE";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
+
 
 //action creators
 export function addMovies(movies) {
@@ -15,3 +19,22 @@ export function addMovies(movies) {
     movies
   };
 }
+export function addFavourite(movie) {
+  return {
+    type: ADD_To_FAVOURITE,
+    movie
+  };
+}
+export function removeFromFavourites(movie){
+  return {
+    type: REMOVE_FROM_FAVOURITE,
+    movie
+  };
+}
+export function setShowFavourites(val){
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val
+  };
+}
+
